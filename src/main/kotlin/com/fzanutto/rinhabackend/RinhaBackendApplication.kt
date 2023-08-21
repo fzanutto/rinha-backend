98c0532b33a37ed1c19e4106b49208ae12765b22
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-21 14:09:11 -0300

    setup project

---
+++ src/main/kotlin/com/fzanutto/rinhabackend/RinhaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.fzanutto.rinhabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RinhaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<RinhaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/fzanutto/rinhabackend/RinhaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fzanutto.rinhabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
