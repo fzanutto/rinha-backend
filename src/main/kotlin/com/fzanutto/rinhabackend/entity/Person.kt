fad2df7e00a5b2c4e8671a4c117447eee2c554d6
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-21 18:47:47 -0300

    connect to database

--- src/main/kotlin/com/fzanutto/rinhabackend/entity/Person.kt
+++ src/main/kotlin/com/fzanutto/rinhabackend/entity/Person.kt
@@ -3,16 +3,20 @@ package com.fzanutto.rinhabackend.entity
 import jakarta.persistence.Column
 import jakarta.persistence.Convert
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
-import java.util.*
+import java.util.Date
+import java.util.UUID
 
 @Entity
 @Table
 class Person(
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     var id: UUID = UUID.randomUUID(),
     @Column(unique = true, length = 32)
     @NotBlank
