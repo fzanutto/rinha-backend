159febc7786eab533f5a18f6443f133993f7ead0
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-23 19:01:51 -0300

    simulate remove redis to test

--- src/main/kotlin/com/fzanutto/rinhabackend/PersonService.kt
+++ src/main/kotlin/com/fzanutto/rinhabackend/PersonService.kt
@@ -11,7 +11,6 @@ import kotlin.jvm.optionals.getOrNull
 class PersonService(
     private val personRepository: PersonRepository
 ) {
-    @Cacheable("person")
     fun getPersonById(uuid: UUID): PersonEntity? {
         return personRepository.findById(uuid).getOrNull()
     }
